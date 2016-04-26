fb860e3f48e0f862609f88ac619fbfdd33987e70
ursaw <urs.w@gmx.de>
2016-04-26 22:05:43 +0200

    update of ensfr page according to saison 2016

--- sites/ensfr2015/content/bundesliga.cntnt.php
+++ sites/ensfr2015/content/bundesliga.cntnt.php
@@ -2,38 +2,63 @@
 <h1>Der EnSFr bei der Segelbundesliga</h1>
 
 <p>
-Als die Entdecker- und Seefahrer-Fördervereinigung im Jahr 2014
-zur Relegation in Glücksburg antrat, da glaubte so manche Crew, gegen
-diese „Kap Hoorniers“ leichtes Spiel zu haben. Doch weit gefehlt! Denn
-die Gründerväter und -mütter des Vereins sind keine alternden
-Frachtboot-Segler, sondern entstammen der mit reichlich innovativem
-Geist und kämpferischer Natur ausgestatteten OK-Jollen-Szene. Sie gaben
-in der Zeit der AKW-Demos und Grünen-Gründung denjenigen eine Heimat,
-die der Spießigkeit in den traditionellen Vereinen überdrüssig waren.
+Die Entdecker kommen.... mit eine Legende am Lenker. Frank Schönfeld, der singende 
+vielfache Meister, startet in diesem Jahr für die Entdecker- und Seefahrer Fördervereinigung. 
+Immerhin ist der Meister zahlreicher Klassen schon seit OK­-Zeiten bei den Entdeckern und 
+Gründungsmitglied. Entscheidend geprägt haben ihn die langhaarigen OK-­Segler aus 
+Övelgönne um die Jungblut­-Brüder mit dem Spruch wo Flaute zu Recht wird (Auslaufen bei 
+Null­-Wind), wird Widerstand zur Pflicht.
 </p>
 
 <p>
-Heute bilden starke 505er-Segler, Match-Racer und Dickschiff-Segler den
-Kern, und so schaffte der Verein den Sprung in die Zweite Liga und dort
-souverän den sicheren Klassenerhalt.
+Der Aderlass der Entdecker, Spitzensegler Andreas Willi Willim segelt in diesem Jahr mit 
+seinem Schleswiger Familien­-Team (u.a. Ehefrau und Söhne) lies den Mannschaftsrat um 
+Tom Gosch und Klaus Stammerjohann aktiv und vor allem kreativ werden. Sie überzeugten 
+Frank bei zahlreichen Bieren in der Linde. 
 </p>
 
 <p>
- „Wir bauen weiter auf den Kader der
-vergangenen Saison und haben ein paar alte Match-Racer um Andreas Willim
-reaktivieren können. Die Breite an Vorschiffsleuten ist hoch, bei den
-Steuermännern müssen wir genau planen. Stolz sind wir auf die
-zahlreichen guten Frauen im Team“, sagt Team-Manager Klaus
-Stammerjohann, der den frühen Klassenerhalt als Ziel und die
-Qualifikation für die Erste Liga als Traum formuliert. Ohne eigenes
-Trainingsboot, aber mit der Leihgabe einer J/70 von Frank Schönfeldt
-bereiten sich die „Entdecker und Seefahrer“ gemeinsam mit der Hamburger
-Trainingsgemeinschaft, wenn auch nur kurzfristig, auf die Saison. Die
-wird der Verein oft in unterschiedlicher Besetzung bestreiten – bedingt
-durch die anderen Regatta-Aktivitäten der Team-Mitglieder und berufliche
-Belastungen.
+Frank selbst war dann sofort Feuer und Flamme und stellte finanzielle Forderungen andere 
+Art: “Wir nehmen nicht, wir geben” ist der Slogan der Saison und so wollen die Entdecker bei 
+Erfolg für gute Zwecke spenden. An wen und wieviel entscheidet die Mannschaft am 
+Spieltag. Die ersten Spenden gehen an das Tierheim Süderstraße in Hamburg und an die 
+Essenstafel Westerrönfeld. 
 </p>
 
+<p>
+Wer sind die Entdecker: Tom Gosch, ehemaliger Deutscher Meister in der OK und im 
+Match­-Race drückt es so aus: “Gute attraktive Steuerleute aus der OK und querdenkende 
+agile Vorschoter aus dem 505­er. Dazu Strandgut und die schönsten Vorschoterinnen”. 
+</p>
+
+<p>
+Dieser kleine Verein aus Hamburg (knapp über 30 Mitglieder aus Norddeutschland) möchte 
+die Etablierten ärgern. Dazu dient etwas Training und viel Erfahrung. So munkelt man, dass 
+die Entdecker der erfolgreichste Seglerverein Deutschlands ist: mit den meisten Meistertiteln 
+pro Mitglied im Schnitt. Klar, oder? 
+</p>
+
+<p> 
+Wer ist 2016 im Team: Neben Teammanager Klaus Stammerjohann sind dies bei 
+Entdecker1 (Horst): Dietrich Scheder, Urs Wihlfahrt und Fritz Schlomka und bei Entdecker2 
+(Angela) neben dem Clown, Jörn Richter, Matten Jaetschke und Andreas Schmidt. Dazu 
+kommen noch Tom Gosch (Rekonvaleszent), Antje Gosch und Suse B. 
+</p>
+
+<p>
+Das Ziel für die Saison ist ein es, die Liga zu halten. Dazu wird die J70 von Frank auf der 
+Alster als Trainingsboot genutzt. 
+</p>
+
+<p>
+In Starnberg segeln die Entdecker 2 mit dem Clown an der Pinne. Das Ziel der neu 
+zusammen gesetzten Mannschaft ist es, ich weit von den Abstiegsrängen fern zu halten. 
+Denn im letzten Jahr legten die Entdecker mit dem letzten Platz in Starnberg einen 
+absoluten Fehlstart hin. Wer sind die anderen Segler: Taktik macht der erfahrene OK und 
+505 Segler Matten Jetschke aus Bremen, Trimmer ist der OK­-Segler Jörn Richter. Das Vorschiff wird von dem Kieler ehemaligen 505 Segler und Journalist Andreas Schmidt 
+besetzt. 
+</p> 
+ 
 
 <h1>Leistungskurve:</h1>
 
@@ -41,5 +66,6 @@ Belastungen.
   <dt>2013</dt>  <dd>nicht angetreten</dd>
   <dt>2014</dt>  <dd>Relegation: Platz 9</dd>
   <dt>    </dt>  <dd>Zweite Liga: Platz 8</dd>
-  <dt>Ziel 2015</dt>  <dd>Klassenerhalt sichern</dd>
+  <dt>2015</dt>  <dd>Zweite Liga: Platz 11</dd>
+  <dt>Ziel 2016</dt>  <dd>Klassenerhalt sichern</dd>
 </dl>
